4951081aa9b2463e34d8efab4f48b8cfafb48e9f
Neibagha Kingsway ThankGod <115288486+ItsThankK@users.noreply.github.com>
2024-05-25 10:21:48 +0000

    load keypair from .env file

--- Lesson_2/generate-keypair.ts
+++ Lesson_2/generate-keypair.ts
@@ -3,6 +3,9 @@ import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
 const loadedKeypair = getKeypairFromEnvironment("SECRET_KEY");
 
+console.log("\n");
 console.log(`Public key ---> `, loadedKeypair.publicKey.toBase58());
 console.log("\n");
 console.log(`Secret key ---> `, loadedKeypair.secretKey);
+console.log("\n");
+console.log("Successfully loaded keypair form .emv file!!! 👍");
